ffd9d771bc1dc6b5ac7c0a9b54ba310ee0098b37
GuadalupeParogua <lupeparogua@gmail.com>
2022-09-26 22:42:28 -0400

    model, controller, template

---
+++ app/Http/Controllers/salaController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class salaController extends Controller
+{
+    //
+}
--- database/migrations/2022_09_20_000000_personas.php
+++ database/migrations/2022_09_20_000000_personas.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->date('fecha_nacimiento');
             $table->unsignedInteger('telefono');
             $table->text('direccion');
+            $table->char('tipo',1);
 
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
