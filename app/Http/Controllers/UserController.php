c9001240ba4cb668b60d9998458e5c08755c401d
GuadalupeParogua <lupeparogua@gmail.com>
2022-10-09 23:56:13 -0400

    implementacion usuario

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\administrativo;
+use App\Models\doctor;
+use App\Models\paciente;
+use App\Models\persona;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use PhpParser\Node\Stmt\Break_;
 
 class UserController extends Controller
 {
@@ -43,11 +48,78 @@ class UserController extends Controller
         return view('gestionar_usuario.index')->with('usuarios', $usuarios);
     }
 
-    public function edit($user_id)
+    public function show($persona_id){
+        $persona = persona::find($persona_id);
+        $administrativo = administrativo::all();
+        $paciente = paciente::all();
+        $doctor = doctor::all();
+        $user = User::all(); 
+        $persona->load('administrativo');
+        $persona->load('paciente');
+        $persona->load('doctor');
+        $persona->load('user');
+       return view('gestionar_usuario.show', compact('persona'));
+    }
+    public function edit($persona_id)
     {
-      $usuarios = User::findOrFail($user_id);
-      $usuarios->load('usuario');
-      return view('gestionar_usuario.edit', ['usuarios' => $user_id]);
+        $persona = persona::find($persona_id);
+        $administrativo = administrativo::all();
+        $paciente = paciente::all();
+        $doctor = doctor::all();
+        $user = User::all(); 
+        $persona->load('administrativo');
+        $persona->load('paciente');
+        $persona->load('doctor');
+        $persona->load('user');
+       return view('gestionar_usuario.edit', compact('persona'));
+    }
+    public function update(Request $request, $persona_id){
+        $request->validate([
+            'name'=>'required',
+            'password'=>'required',
+        ]);
+        $persona_usuario = User::where('name', $request->name)->where('persona_id', '!=', $persona_id)->first();
+        if (!is_null($persona_usuario)) {
+        return back()->withErrors(['El usuario ya existe, intente con otro']);
+        }
+        //$persona = persona::find($persona_id);
+        $user = User::where('persona_id',$persona_id)->first();
+        $user->name = $request->name;
+        //$user->email = $request->email;
+        $user->password= bcrypt($request->password);
+        $user->update();
+        return redirect()->route('usuario.index');
+    }
+
+    public function destroy($persona_id)
+    {
+        $persona = persona::findOrFail($persona_id);
+        
+        //$user = User::where('persona_id',$persona_id)->first();
+        $usuario= $persona->user;
+        $usuario->delete();
+        
+        switch ($persona->tipo[0]) {
+             case 'A':
+                 $administrativo = administrativo::where('persona_id',$persona_id)->first();
+                 $administrativo->delete($administrativo);
+                 break;
+             case 'P':                
+                 $paciente = paciente::where('persona_id',$persona_id)->first();
+                 $paciente->delete($paciente);
+                 break;
+             case 'D':
+                 $doctor = doctor::where('persona_id',$persona_id)->first();
+                 $doctor->delete($doctor);
+                 break;
+             
+             default:
+                 # code...
+                 break;
+         }
+        $persona->delete(); 
+
+        return redirect()->route('usuario.index');
     }
 
 }
--- resources/views/gestionar_usuario/index.blade.php
+++ resources/views/gestionar_usuario/index.blade.php
@@ -7,8 +7,6 @@
                 <div class="container-fluid">
                     <!-- Page Heading -->
                     
-                    <p class="mb-4"></p>       
-
                     <div class="my-2"></div>
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
@@ -21,38 +19,56 @@
                                     <thead>
                                         <tr>
                                             <th>Nrº</th>
+                                            <th>CI</th>
                                             <th>Nombre</th>
-                                            <th>Usuario</th><!--unique-->
-                                            <th>Acciones</th><!--unique-->
+                                            <th>Usuario</th>
+                                            <th>Tipo</th>
+                                            <th>Acciones</th>
                                         </tr>
                                     </thead>
                                     <tfoot>
                                         <tr>
                                             <th>Nrº</th>
+                                            <th>CI</th>
                                             <th>Nombre</th>
-                                            <th>Usuario</th><!--unique-->
+                                            <th>Usuario</th>
+                                            <th>Tipo</th>
                                             <th>Acciones</th>
                                         </tr>
                                     </tfoot>
                                     <tbody>
                                         @php
                                             $i = 1;
+         
                                         @endphp
 
                                         @foreach ($usuarios as $usuario)
                                          
                                             <tr>
                                                 <td>{{ $i++ }}</td>
+                                                <th>{{$usuario->persona->ci}}</th>
+                                                <th>{{$usuario->persona->nombre." ".$usuario->persona->apellido_paterno . " " . $usuario->persona->apellido_materno }}</th>
                                                 <th>{{$usuario->name}}</th>
-                                                <th>{{$usuario->name}}</th><!--nullable-->
+                                                <th>{{$usuario->persona->tipo}}</th>
                                                 <td>
-                                                    <form action="{{ route('administrativo.destroy', [$usuario->id]) }}" method="post">
+                                                    <form action="{{route('usuario.destroy',$usuario->persona_id)}}" method="post">
                                                         @csrf
                                                         @method('delete')
-                                                        <a href="{{ route('administrativo.edit', [$usuario->id]) }}"
+                                                        @if($usuario->persona->tipo[0]=='A')
+                                                        <a href="{{route('administrativo.show',$usuario->persona_id)}}" class="btn btn-info btn-sm fas fa-eye cursor-pointer"></a>
+                                                        @endif
+                                                        @if($usuario->persona->tipo[0]=='P')
+                                                        <a href="{{route('paciente.show',$usuario->persona_id)}}" class="btn btn-info btn-sm fas fa-eye cursor-pointer"></a>
+                                                        @endif
+                                                        @if($usuario->persona->tipo[0]=='D')
+                                                        <a href="{{route('paciente.show',$usuario->persona_id)}}" class="btn btn-info btn-sm fas fa-eye cursor-pointer"></a>
+                                                        @endif
+                                                        
+                                                        <a href="{{route('usuario.edit',$usuario->persona_id)}}"
                                                             class="btn btn-primary btn-sm fas fa-edit  cursor-pointer"></a>
                                                         <button class="btn btn-danger btn-sm fas fa-trash-alt  cursor-pointer"
-                                                            onclick="return confirm('¿ESTA SEGURO DE  BORRAR?')" value="Borrar"> </button>
+                                                            onclick="return confirm('¿ESTA SEGURO QUE DESEA ELIMINAR?')" value="Borrar">
+                                                        </button>
                                                     </form>
                                                 </td>
                                             </tr>
