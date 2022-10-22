fe49a96b09464966d99a71bc72ccc75bb9350aaa
GuadalupeParogua <lupeparogua@gmail.com>
2022-10-22 19:20:07 -0400

    modificacion administrativo-paciente-lang

--- lang/en - copia/passwords.php
+++ lang/en - copia/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Las contraseñas deben coincidir y contener al menos 6 caracteres',
-    'reset'    => '¡Tu contraseña ha sido restablecida!',
-    'sent'     => '¡Te hemos enviado por correo el enlace para restablecer tu contraseña!',
-    'token'    => 'El token de recuperación de contraseña es inválido.',
-    'user'     => 'No podemos encontrar ningún usuario con ese correo electrónico.',
+    'reset' => 'Your password has been reset!',
+    'sent' => 'We have emailed your password reset link!',
+    'throttled' => 'Please wait before retrying.',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that email address.",
 
 ];
